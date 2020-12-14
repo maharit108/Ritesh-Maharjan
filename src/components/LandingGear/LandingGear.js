1b99888327dfe59861d8e36d84131b29ec5e5e70
maharit108 <maha.rit108@gmail.com>
2020-12-13 23:21:10 -0600

    className changed as it interfered with bootstrap names

--- src/components/LandingGear/LandingGear.js
+++ src/components/LandingGear/LandingGear.js
@@ -9,32 +9,32 @@ const LandingGear = () => {
   // const [isMouseOnMainMenu, setMainMenu] = useState(false)
 
   return (
-    <div id='landing' className='landing'>
-      <div className='abc' onMouseOut={() => setHover(false)}></div>
+    <div id='gearLanding' className='gearLanding'>
+      <div className='outerGearBox' onMouseOut={() => setHover(false)}></div>
 
-        <div className='wrapper' onMouseEnter={() => setHover(true)} >
-          <div className='box' >
-            <img className='img' src={pho} alt='ritesh Mmharjan img' />
+        <div className='gearWrapper' onMouseEnter={() => setHover(true)} >
+          <div className='gearBox' >
+            <img className='gearImg' src={pho} alt='ritesh Mmharjan img' />
           </div>
         </div>
 
-      <div className='menu' style= {isHover ? {transform: `scale(${1})`} : {transform: `scale(${0.9})`}}>
-        <div className='item' style= {isHover ? {transform: `translateY(${-160}px)`} : {transform: `translateY(${0}px)`}} onMouseOver={() => setHover(true)}>
+      <div className='gearMenu' style= {isHover ? {transform: `scale(${1})`} : {transform: `scale(${0.9})`}}>
+        <div className='gearItem' style= {isHover ? {transform: `translateY(${-160}px)`} : {transform: `translateY(${0}px)`}} onMouseOver={() => setHover(true)}>
           <a href='#aboutGear'>1</a>
         </div>
-        <div className='item' style= {isHover ? {transform: `translate(${140}px, ${-80}px)`} : {transform: `translate(${0}px)`}}>
+        <div className='gearItem' style= {isHover ? {transform: `translate(${140}px, ${-80}px)`} : {transform: `translate(${0}px)`}}>
           <a href='#skillsGear'>2</a>
         </div>
-        <div className='item' style= {isHover ? {transform: `translate(${140}px, ${80}px)`} : {transform: `translate(${0}px)`}}>
+        <div className='gearItem' style= {isHover ? {transform: `translate(${140}px, ${80}px)`} : {transform: `translate(${0}px)`}}>
           <a href='#projectsGear'>3</a>
         </div>
-        <div className='item' style= {isHover ? {transform: `translateY(${160}px)`} : {transform: `translateY(${0}px)`}}>
+        <div className='gearItem' style= {isHover ? {transform: `translateY(${160}px)`} : {transform: `translateY(${0}px)`}}>
           <a href='#storyGear'>4</a>
         </div>
-        <div className='item' style= {isHover ? {transform: `translate(${-140}px, ${80}px)`} : {transform: `translate(${0}px)`}}>
+        <div className='gearItem' style= {isHover ? {transform: `translate(${-140}px, ${80}px)`} : {transform: `translate(${0}px)`}}>
           <a href='#galleryGear'>5</a>
         </div>
-        <div className='item' style= {isHover ? {transform: `translate(${-140}px, ${-80}px)`} : {transform: `translate(${0}px)`}}>
+        <div className='gearItem' style= {isHover ? {transform: `translate(${-140}px, ${-80}px)`} : {transform: `translate(${0}px)`}}>
           <a href='#contactsGear'>6</a>
         </div>
       </div>
@@ -42,8 +42,3 @@ const LandingGear = () => {
   )
 }
 export default LandingGear
-
-// <div className='brief'>
-//   <h2 className='name'>RITESH MAHARJAN</h2>
-//   <h3 className='intro'>Full Stack Software Developer</h3>
-// </div>
