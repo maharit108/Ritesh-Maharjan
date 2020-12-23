1c74b18458e2fe1fc9bbea8ceead1f429a3a10dc
maharit108 <maha.rit108@gmail.com>
2020-12-22 22:30:38 -0600

    projectData file added

---
+++ src/components/projectData/projectData.js
@@ -0,0 +1,15 @@
+import pho from './../Landing/photo.jpg'
+
+const projectData = [
+    {
+        title: 'User Stories',
+        info: 'abcde',
+        image: pho
+    },
+    {
+        title: 'abcdeyo',
+        info: 'abcdeyo'
+    }
+]
+
+export default projectData
