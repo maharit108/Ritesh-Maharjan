3b915a8d1f8ddc2b1690fb9c3ebc6e9fdff8aef2
maharit108 <maha.rit108@gmail.com>
2020-12-22 22:29:40 -0600

    Timeline component incorporated to App
    Routes not working proper

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from 'react'
+import { Route } from 'react-router-dom'
 
 import './App.css'
 
@@ -11,6 +12,7 @@ import About from '../About_Me/About.js'
 import Skills from '../Skills/Skills.js'
 import Project from '../Projects/Project.js'
 import Contact from '../Contacts/Contact.js'
+import Timeline from '../Timeline/Timeline.js'
 
 import LandingGear from '../LandingGear/LandingGear.js'
 
@@ -25,25 +27,33 @@ function App() {
         <LandingGear />
       ) : (
         <>
-        <Home />
-        <Navbar bg="dark" variant="dark" expand="md" sticky="top" expanded={expanded}>
-          <Navbar.Brand href='#'> RITESH MAHARJAN </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(expanded ? false : "expanded")} />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto">
-            <Nav.Link onClick={() => setExpanded(false)} href="#home">HOME</Nav.Link>
-            <Nav.Link onClick={() => setExpanded(false)} href="#about">ABOUT</Nav.Link>
-            <Nav.Link onClick={() => setExpanded(false)} href="#skills">SKILLS</Nav.Link>
-            <Nav.Link onClick={() => setExpanded(false)} href="#projects">PORTFOLIO</Nav.Link>
-            <Nav.Link onClick={() => setExpanded(false)} href="#contacts">CONTACTS</Nav.Link>
-            <Nav.Link onClick={() => setExpanded(false)} href={resume} target="_blank">RESUME</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-        <About />
-        <Skills />
-        <Project />
-        <Contact />
+         < Route exact path='/' render ={() => (
+           <>
+          <Home />
+          <Navbar bg="dark" variant="dark" expand="md" sticky="top" expanded={expanded}>
+            <Navbar.Brand href='#'> RITESH MAHARJAN </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(expanded ? false : "expanded")} />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ml-auto">
+              <Nav.Link onClick={() => setExpanded(false)} href="#home">HOME</Nav.Link>
+              <Nav.Link onClick={() => setExpanded(false)} href="#about">ABOUT</Nav.Link>
+              <Nav.Link onClick={() => setExpanded(false)} href="#skills">SKILLS</Nav.Link>
+              <Nav.Link onClick={() => setExpanded(false)} href="#projects">PORTFOLIO</Nav.Link>
+              <Nav.Link onClick={() => setExpanded(false)} href="#contacts">CONTACTS</Nav.Link>
+              <Nav.Link onClick={() => setExpanded(false)} href={resume} target="_blank">RESUME</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+          <About />
+          <Skills />
+          <Project />
+          <Contact />
+          </>
+        )} />
+        
+        < Route exact path='/projects/:projectName' render ={() => (
+          <Timeline />
+        )} />
         </>
       )}
     </Fragment>
