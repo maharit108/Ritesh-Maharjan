db4926bf264bdfd7e6172c70736d9c987ffd7abb
maharit108 <maha.rit108@gmail.com>
2020-12-22 22:28:57 -0600

    TimelineItems component added and style added

--- src/components/TimelineItem/TimelineItem.js
+++ src/components/TimelineItem/TimelineItem.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
+import './TimelineItem.css'
 function TimelineItem({ data }) {
+    console.log(data)
     return (
         <div className='timelineitem'>
             <div className='timelineitem__content'>
@@ -8,7 +10,7 @@ function TimelineItem({ data }) {
                     {data.title}
                 </span>
                 <p>{data.info}</p>
-                {data.image && (<img src='data.image' alt='image' />)}
+                {data.image && (<img src={data.image} alt='' />)}
             </div>
             <span className='circle'></span>
         </div>
