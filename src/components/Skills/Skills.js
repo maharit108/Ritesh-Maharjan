d5ea17b2777c03ceb7d6feeeb195ff2db79f57cd
maharit108 <maha.rit108@gmail.com>
2021-02-11 23:36:48 -0600

    Skills updated

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -9,23 +9,23 @@ const Skills = () => (
         <h1>MY SKILLS</h1>
         <div className='row-in'>
           <h2 className='skillTitle'>Languages & Frameworks</h2>
-          <div className='sk'>React, JavaScript, JQuery, HTML/CSS, Bootstrap, Express, AJAX, Axios, Python, Django, Socket.io, </div>
+          <div className='sk'>React, JavaScript, JQuery, HTML/CSS, Express, Python, Django, Mongoose, Socket.io, Mapbox, jest</div>
         </div>
 
-          <div className='row-in'>
-            <h2 className='skillTitle'>Management & Environment</h2>
-            <div className='sk'>Git, Github, Heroku, Node.js, Firebase</div>
-          </div>
+        <div className='row-in'>
+          <h2 className='skillTitle'>Platform & Environment</h2>
+          <div className='sk'>Git, Github, Heroku, Node.js, Firebase</div>
+        </div>
 
-            <div className='row-in'>
-              <h2 className='skillTitle'>Methodologies</h2>
-              <div className='sk'>Object Oriented Programming, MVC, Responsive Design, Authentication, User Stores, Wireframing, ERD's, Agile Development, Scrum, RESTful api, Single Page Applications</div>
-            </div>
+        <div className='row-in'>
+          <h2 className='skillTitle'>Methodologies</h2>
+          <div className='sk'>Object Oriented Programming, Functional Programming, MVC, Test-Driven development, Responsive Design, RESTful API, Authentication, User Stores, Wireframing, ERD's, Agile/Scrum, Kanban/Trello </div>
+        </div>
 
-              <div className='row-in'>
-                <h2 className='skillTitle'>Databases</h2>
-                <div className='sk'>MongoDB, PostgreSQL, Firebase </div>
-              </div>
+        <div className='row-in'>
+          <h2 className='skillTitle'>Databases</h2>
+          <div className='sk'>MongoDB, PostgreSQL, Firebase </div>
+        </div>
     </div>
   </div>
 )
